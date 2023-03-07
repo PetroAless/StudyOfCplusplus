6f1ce6bd97868703dfa7222a4ca9674c2daae2ce
Alessio Petrotta <alessio.petrotta1@gmail.com>
2023-03-08 00:54:37 +0100

    now gonna work on studying, added json to roadmap

--- main.cpp
+++ main.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
+#include <string>
 using namespace std;
 void spacing(){
 for(int i = 0 ; i<5 ; i++){
         cout << endl;
     }
 }
-int main(){
-    
+int main(int argc, char* argv[]){
+
     cout << "Hello World!" << endl;
-    cout << 13 << endl;
-    spacing();
+    cout << 13 << endl ;
+    
+    
+
+
+
+
+
+
+
+
+
+    string c="default";
+    cout << endl  << endl  << endl  << endl << "enter anything to end";
+    while(c=="default"){
+        cin >> c;
+    }
+    return 0;
 }
