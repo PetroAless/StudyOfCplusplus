c56fb296b6fd4612f5b12a7b45c44787918ed155
Alessio Petrotta <alessio.petrotta1@gmail.com>
2023-03-10 17:29:11 +0100

    notes updated, tried things on pointer arrays

--- Project 1/main.cpp
+++ Project 1/main.cpp
@@ -10,18 +10,27 @@ void pointer(){
         cout << j << " element of array:" << *i << endl;
     }
 }
-void test(int *a,int *b){
-    cout << "a,b:" << *a << "," << *b << endl;
-    *a = 10;*b = 20;
+void swap(int *a,int *b){
+    *a=*a+*b;
+    *b=*a-*b;
+    *a=*a-*b;
 }
-
+int sum(int a,int b){
+    return a+b;
+}
 int main(int argc, char *argv[]){
     //pointer();
-    int a=10;
-    int *b=&a;
-    int *x = new int(10);
-    printf("address of x:%d, value *x:%d\n",x,*x);
-    printf("address of b:%d, value *b=&a:%d\n",b,*b);
+    int *arr=new int[8];
+    for(int i=0;i<8;i++){
+        arr[i]=i+(rand()%8)+2;
+        cout << "arr["<<i<<"]:" << arr[i] << endl;
+    }
+    
+    
+    for(int i=0;i<sizeof(arr);*arr++,i++){
+        printf("*arr:%d\n",*arr);
+    }
+    
     //cout << "\nenter anything to end:\n";getch();
     return 0;
 }
